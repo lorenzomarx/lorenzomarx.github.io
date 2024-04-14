fb877b2d492a01132e87af826ea80c3ae866ee9b
lorenzomarx <bookings.lorenz@gmail.com>
2024-04-14 21:41:07 +1200

    =Updates

--- newsly/api/breaking_us/breaking_us.py
+++ newsly/api/breaking_us/breaking_us.py
@@ -5,9 +5,9 @@ from colorama import colorama_text, Fore, Back, Style
 import pyfiglet
 result = pyfiglet.figlet_format("News", font = "poison"  )
 
-response = requests.get("https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=3b0812f373dd4cf6be98999b0bb159b4")
+response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=3b0812f373dd4cf6be98999b0bb159b4")
 data = response.json()
 
 
-with open('natgeo.json', 'w') as outfile:
+with open('breaking.json', 'w') as outfile:
     json.dump(data, outfile,indent=2, sort_keys=True)
