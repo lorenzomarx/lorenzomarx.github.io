df22f1ef3aa2a0839da272417cf95ec44a406d19
lorenzomarx <bookings.lorenz@gmail.com>
2024-10-28 08:12:20 +1300

    =Updates

--- newsly/weather.js
+++ newsly/weather.js
@@ -1,4 +1,4 @@
-let urlNews = "https://lorenzomarx.github.io/newsly/api/aljazApi/aljaz.json";
+var urlNews = "https://lorenzomarx.github.io/newsly/api/aljazApi/aljaz.json";
 
 function setup() {
   noCanvas();
@@ -8,7 +8,6 @@ function setup() {
 }
 
 function gotData(data) {
-  console.log(data.articles);
   let articles = data.articles;
 
   // Choose a random article
@@ -18,6 +17,7 @@ function gotData(data) {
   // Create an image element for the randomly selected article
   let image = createImg(randomArticle.urlToImage, "Random Article Image");
   image.addClass('img-fluid z-depth-3 p-3 flex-1');
+  imageMode(CENTER);
   image.parent('result');
 }
 
