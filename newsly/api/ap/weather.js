cd0ad387c28dcc5a84c5fb7e0ecb88ee1cd87c4f
lorenzomarx <bookings.lorenz@gmail.com>
2026-05-18 18:33:24 +1200

    Updates

--- newsly/api/ap/weather.js
+++ newsly/api/ap/weather.js
@@ -1,27 +1,27 @@
-
-var urlNews = "https://lorenzomarx.github.io/newsly/api/ap/ap.json";
+const urlNews = "https://lorenzomarx.github.io/newsly/api/ap/ap.json";
 
 function setup() {
-
- loadJSON(urlNews, gotData);
-
+    noCanvas();
+    loadJSON(urlNews, render, onError);
 }
 
- function gotData(data) {
-   var NY = data.articles;
-   var SRC = data.source;
-   for (var i = 0; i < NY.length; i++) {
-     //createElement('h3',articles[i].section);
-     let author = createElement('h2',NY[i].author);
-     author.parent('result');
-     let title = createElement('h3',NY[i].title).addClass('card');
-     title.parent('result');
-     let par = createP(NY[i].description);
-     par.parent('result');
-     let image = createImg(NY[i].urlToImage).addClass('img-fluid z-depth-3 p-3 flex-1');
-     image.parent('result');
-     let url = createA(NY[i].url,"Link",'_blank').addClass('btn btn-outline-primary waves-effect');
-     url.parent('result');
+function render(data) {
+    const articles = (data && data.articles) || [];
+    if (!articles.length) {
+        onError();
+        return;
+    }
+    articles.forEach(a => {
+        const card = createDiv('').addClass('article-card');
+        card.parent('result');
+        if (a.author)      createElement('span', a.author).addClass('author').parent(card);
+        if (a.title)       createElement('h2', a.title).addClass('title').parent(card);
+        if (a.urlToImage)  createImg(a.urlToImage, a.title || '').addClass('news-img').parent(card);
+        if (a.description) createElement('p', a.description).addClass('description').parent(card);
+        if (a.url)         createA(a.url, 'Read →', '_blank').addClass('read-btn').parent(card);
+    });
+}
 
-   }
- }
+function onError() {
+    createP('Feed unavailable. Try again later.').parent('result').addClass('error-message');
+}
